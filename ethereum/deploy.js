0a987791e8d5ae9c9a054b07b0ee2458d1794df8
raj-raut8502 <raj_raut@hotmail.com>
2024-04-16 09:12:30 +0530

    initial commit

---
+++ ethereum/deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledRecord = require('./build/Record.json');
+
+//Link to rinkeby network by using Infura and providing seed phrase of metamask wallet
+const provider = new HDWalletProvider(
+    'tag siege affair found behave welcome violin phrase skull panther club hammer',
+    'HTTP://127.0.0.1:7545',
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    //Deploy contract to rinkeby network
+    const result = await new web3.eth.Contract(JSON.parse(compiledRecord.interface))
+        .deploy({ data: compiledRecord.bytecode })
+        .send({ gas: '10000000', from: accounts[0] });
+
+    //Display the address of the contract 
+    console.log('Contract deployed to', result.options.address);
+
+    //Always go to record.js after updating solidity code
+};
+
+deploy();
